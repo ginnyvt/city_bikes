9cc88d17d2777f6edd41bc9e81c0224324bc9800
ginnyvt <ginny@powerkernel.com>
2023-02-05 15:53:57 +0200

    feat: add google map for single station view

---
+++ client/src/components/Map.tsx
@@ -0,0 +1,33 @@
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+
+const googleMapsApiKey = process.env.REACT_APP_MAPS_API;
+console.log(googleMapsApiKey);
+
+interface MapProps {
+	lat: number;
+	lng: number;
+}
+export default function Map(props: MapProps) {
+	const { isLoaded } = useJsApiLoader({
+		id: "google-map-script",
+		googleMapsApiKey: googleMapsApiKey!,
+	});
+
+	const containerStyle = {
+		width: "100%",
+		height: "100%",
+	};
+
+	const center = {
+		lat: props.lat,
+		lng: props.lng,
+	};
+
+	return isLoaded ? (
+		<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+			<Marker position={center} />
+		</GoogleMap>
+	) : (
+		<></>
+	);
+}
